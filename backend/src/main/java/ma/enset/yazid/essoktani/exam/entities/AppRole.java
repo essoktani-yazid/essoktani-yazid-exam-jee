d1ec44e35bafcd8965547aee7cf5943ccc2a8722
ESSOKTANI Yazid <essoktani.yazid@gmail.com>
2026-05-11 14:12:17 +0100

    feat(backend): initialize spring boot project structure

---
+++ backend/src/main/java/ma/enset/yazid/essoktani/exam/entities/AppRole.java
@@ -0,0 +1,12 @@
+package ma.enset.yazid.essoktani.exam.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class AppRole {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roleName;
+}
